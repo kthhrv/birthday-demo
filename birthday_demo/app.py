de602a77761b2fde85ff81b277830e9f9582e7b3
Keith Harvey <keith.harvey@timeout.com>
2023-09-11 16:31:09 +0100

    tidy

--- birthday_demo/app.py
+++ birthday_demo/app.py
@@ -4,15 +4,15 @@ import datetime
 
 from flask import Flask, jsonify, request
 
-from . import datastore
+from . import datastore, utils
 
 app = Flask(__name__)
 
 
-@app.route('/hello/<username>', methods=['PUT'])
-def create_user(username: str) -> str:
+@app.route('/hello/<username>', methods=['GET', 'PUT'])
+def endpoint(username: str) -> str:
     '''
-    Process a user creation request.
+    Process a username endpoint request
 
        Paramerter:
           username (str): A string containing only letters
@@ -22,43 +22,31 @@ def create_user(username: str) -> str:
     '''
     errors = {}
 
-    dob_str = request.args.get('dateOfBirth')
-    if not dob_str:
-        errors['INVALID_DATE'] = "Please supply 'dateOfBirth' as a valid date in 'YYYY-MM-DD' format"
-    else:
-        try:
-            datetime.datetime.strptime(dob_str, "%Y-%m-%d")
-        except ValueError:
-            errors['INVALID_DATE'] = "Please supply 'dateOfBirth' as a valid date in 'YYYY-MM-DD' format"
-
     if not username.isalpha():
         errors['INVALID_USERNAME'] = "Please supply a 'username' containing only letters"
+
+    if request.method == 'PUT':
+        dob_str = request.args.get('dateOfBirth')
+        if not dob_str:
+            errors['INVALID_DATE'] = "Please supply 'dateOfBirth' as a valid date in 'YYYY-MM-DD' format"
+        else:
+            try:
+                datetime.datetime.strptime(dob_str, "%Y-%m-%d")
+            except ValueError:
+                errors['INVALID_DATE'] = "Please supply 'dateOfBirth' as a valid date in 'YYYY-MM-DD' format"
 
     if errors:
         return jsonify({'errors': errors}), 400
 
     db = datastore.DB()
-    db.create_or_update(username, dob_str)
 
-    return jsonify({}), 204
+    if request.method == 'GET':
+        days = utils.get_days_to_next_birthday(db.get_dob(username))
+        if days == 0:
+            return jsonify({'message': f'Hello, {username}! Happy birthday!'})
+        else:
+            return jsonify({'message': f'Hello, {username}! Your birthday is in {days} day(s)'})
 
-
-@app.route('/hello/<username>', methods=['GET'])
-def check_birthday(username: str) -> str:
-    db = datastore.DB()
-    days = get_days_to_next_birthday(db.get_dob(username))
-
-    if days == 0:
-        return jsonify({'message': f'Hello, {username}! Happy birthday!'})
-    else:
-        return jsonify({'message': f'Hello, {username}! Your birthday is in {days} day(s)'})
-
-
-def get_days_to_next_birthday(dob_str):
-    today = datetime.date.today()
-    dob = datetime.datetime.strptime(dob_str, "%Y-%m-%d")
-    birthday = datetime.date(today.year, dob.month, dob.day)
-    if birthday < today:
-        birthday = birthday.replace(year=today.year + 1)
-
-    return (birthday - today).days
+    elif request.method == 'PUT':
+        db.create_or_update(username, dob_str)
+        return jsonify({}), 204
